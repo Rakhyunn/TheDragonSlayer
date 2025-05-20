e963be873ea6b6c0737cdaf6b07c8af739032276
Rakhyunn <krh0323@pukyong.ac.kr>
2025-05-20 14:58:07 +0900

    [Feature] 기본 적1 구현

---
+++ Source/TheDS/BasicEnemy_1.cpp
@@ -0,0 +1,85 @@
+#include "BasicEnemy_1.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "CharacterAnimInstance.h"
+
+ABasicEnemy_1::ABasicEnemy_1()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+}
+
+void ABasicEnemy_1::BeginPlay()
+{
+	Super::BeginPlay();
+	SetCharacterDefaults();		//이동속도, 점프 설정
+	AnimInstance = Cast<UCharacterAnimInstance>(GetMesh()->GetAnimInstance());	// AnimInstance 가져오기
+	// 함수 등록
+	AnimInstance->OnMontageEnded.AddDynamic(this, &ABasicEnemy_1::OnAttackMontageEnded);
+	AnimInstance->OnAttackHit.AddUObject(this, &ABasicEnemy_1::Attack);
+}
+
+void ABasicEnemy_1::SetCharacterDefaults()
+{
+	walkSpeed = 600.f;
+	runSpeed = 900.f;
+	jumpZVelocity = 500.f;
+
+	GetCharacterMovement()->MaxWalkSpeed = walkSpeed;
+	GetCharacterMovement()->JumpZVelocity = jumpZVelocity;
+}
+
+void ABasicEnemy_1::Attack()
+{
+	Super::Attack();
+
+	if (!bIsAttacking) return;
+	ServerAttack();
+}
+
+void ABasicEnemy_1::ServerAttack_Implementation()
+{
+	if (!HasAuthority()) return;
+	FHitResult hitResult;
+	FCollisionQueryParams params(NAME_None, false, this);
+
+	float attackRange = 100.f;
+	float attackRadius = 50.f;
+
+	bool bHit = GetWorld()->SweepSingleByChannel(OUT hitResult,
+		GetActorLocation(), GetActorLocation() + GetActorForwardVector() * attackRange,
+		FQuat::Identity,
+		ECollisionChannel::ECC_GameTraceChannel2,
+		FCollisionShape::MakeSphere(attackRadius), params);
+
+	FVector forward = GetActorForwardVector() * attackRange;
+	FVector center = GetActorLocation() + forward * 0.5f;
+	float halfHeight = attackRange * 0.5f + attackRadius;
+	FQuat rotation = FRotationMatrix::MakeFromZ(forward).ToQuat();
+
+	FColor DrawColor = bHit ? FColor::Green : FColor::Red;
+
+	DrawDebugCapsule(GetWorld(), center, halfHeight, attackRadius, rotation, DrawColor, false, 2.f);
+	if (bHit && hitResult.GetActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Warrior hit: %s"), *hitResult.GetActor()->GetName());
+	}
+	MulticastAttack();
+}
+
+void ABasicEnemy_1::MulticastAttack_Implementation()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Attack"));
+	// 단순 애니매이션 재생
+	if (AnimInstance)
+	{
+		bIsAttacking = false;
+		AnimInstance->PlayAttackMontage();
+	}
+}
+
+void ABasicEnemy_1::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	bIsAttacking = true;
+}
---
+++ Source/TheDS/BasicEnemy_1.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemyCharacter.h"
+#include "BasicEnemy_1.generated.h"
+
+UCLASS()
+class THEDS_API ABasicEnemy_1 : public ABaseEnemyCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	ABasicEnemy_1();
+
+private:
+	UPROPERTY()
+	class UCharacterAnimInstance* AnimInstance;
+
+private:
+	virtual void BeginPlay() override;
+
+	virtual void SetCharacterDefaults() override;
+
+	virtual void Attack() override;		// 공격 상속 정의
+
+	// 서버, 클라이언트 공격 분리
+	UFUNCTION(Server, Reliable)
+	void ServerAttack();
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastAttack();
+	// 몽타지 종료 시 호출
+	UFUNCTION()
+	void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+};
