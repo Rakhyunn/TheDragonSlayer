7eab1b155961b7985058b29ce0c9fc7d98da89c4
Rakhyunn <krh0323@pukyong.ac.kr>
2025-07-05 23:14:50 +0900

    [Feature] 상점 위젯 구현

---
+++ Source/TheDS/NPCShopWidget.cpp
@@ -0,0 +1,50 @@
+#include "NPCShopWidget.h"
+#include "BaseMerchantNPC.h"
+#include "BaseCharacter.h"
+#include "BaseItem.h"
+#include "BaseStatComponent.h"
+#include "Components/Button.h"
+#include "Components/WrapBox.h"
+#include "Components/TextBlock.h"
+
+void UNPCShopWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (BTN_Close) BTN_Close->OnClicked.AddDynamic(this, &UNPCShopWidget::OnCloseBtnCLicked);
+}
+
+void UNPCShopWidget::InitShop(ABaseMerchantNPC* merchant)
+{
+	MerchantRef = merchant;
+	PlayerRef = Cast<ABaseCharacter>(GetOwningPlayerPawn());
+	RefreshShopItems();
+	UpdateMoney();
+}
+
+void UNPCShopWidget::RefreshShopItems()
+{
+	//if (!WB_ShopItems || !MerchantRef || !ShopSlotWidgetClass) return;
+	WB_ShopItems->ClearChildren();
+	for (UBaseItem* item : MerchantRef->GetItemsForSale())
+	{
+		//UShopItemSlotWidget* slot = CreateWidget< UShopItemSlotWidget>(this, ShopSlotWidgetClass);
+		//if (slot)
+		//{
+		//	slot->Init(item, PlayerRef, MerchantRef);
+		//	WB_ShopItems->AddChildToWrapBox(slot);
+		//}
+	}
+}
+
+void UNPCShopWidget::UpdateMoney()
+{
+	if (PlayerRef && PlayerRef->stat && TXT_PlayerMoney)
+	{
+		TXT_PlayerMoney->SetText(FText::AsNumber(PlayerRef->stat->GetMoney()));
+	}
+}
+
+void UNPCShopWidget::OnCloseBtnCLicked()
+{
+	RemoveFromParent();
+}
---
+++ Source/TheDS/NPCShopWidget.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NPCShopWidget.generated.h"
+
+UCLASS()
+class THEDS_API UNPCShopWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UWrapBox* WB_ShopItems;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* TXT_PlayerMoney;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BTN_Close;
+
+	//UPROPERTY(EditAnywhere, Category = "UI")
+	//TSubclassOf<class UShopItemSlotWidget> ShopSlotWidgetClass;
+
+	UPROPERTY()
+	class ABaseMerchantNPC* MerchantRef;
+
+	UPROPERTY()
+	class ABaseCharacter* PlayerRef;
+	
+public:
+	virtual void NativeConstruct() override;
+
+	void InitShop(class ABaseMerchantNPC* merchant);
+
+	void RefreshShopItems();
+	
+	void UpdateMoney();
+
+	UFUNCTION()
+	void OnCloseBtnCLicked();
+};
