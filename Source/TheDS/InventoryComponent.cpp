eca1a133dfeb2d257e9d188a835bd56ae4228323
Rakhyunn <krh0323@pukyong.ac.kr>
2025-07-24 15:28:48 +0900

    [Fix] 인벤토리 버그 수정
    -소비 아이템 획득 시 0개 아이템 생성되는 오류 수정

--- Source/TheDS/InventoryComponent.cpp
+++ Source/TheDS/InventoryComponent.cpp
@@ -29,16 +29,19 @@ void UInventoryComponent::AddItem(UBaseItem* item, int32 quantity)
 				break;
 		}
 	}
-	for (FInventorySlot& slot : TargetSlots)
+	if (quantity > 0)
 	{
-		if (slot.ItemData == nullptr)
+		for (FInventorySlot& slot : TargetSlots)
 		{
-			int32 toAdd = FMath::Min(quantity, item->maxStack);
-			slot.ItemData = item;
-			slot.Quantity = toAdd;
-			quantity -= toAdd;
-			if (quantity <= 0)
-				break;
+			if (slot.ItemData == nullptr)
+			{
+				int32 toAdd = FMath::Min(quantity, item->maxStack);
+				slot.ItemData = item;
+				slot.Quantity = toAdd;
+				quantity -= toAdd;
+				if (quantity <= 0)
+					break;
+			}
 		}
 	}
 	if (linkedInventoryWidget && linkedInventoryWidget->IsInViewport())
